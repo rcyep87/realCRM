d16f08382ad3bb76b1df7693bca59ccc0aa206d0
Ryan Yep <ryan.yep@gmail.com>
2016-05-15 15:33:28 -0500

    Re-ordered migration files to fit relation needs and added belongs_to in a separate migration file

--- db/migrate/20160515195627_add_belongs_to_city_model.rb
+++ db/migrate/20160515195627_add_belongs_to_city_model.rb
@@ -1,5 +1,6 @@
 class AddBelongsToCityModel < ActiveRecord::Migration
   def change
+    add_column      :cities, :state_id, :integer, null: false
     add_foreign_key :cities, :states
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515194550) do
+ActiveRecord::Schema.define(version: 20160515195627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20160515194550) do
     t.integer  "avg_don"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "state_id",   null: false
   end
 
+  create_table "states", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "population"
+    t.integer  "avg_don"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "cities", "states"
 end
